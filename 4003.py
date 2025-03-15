51019c5b1df302b967debbe34253429263d5d0f1
asp2006 <proud.boy2006@gmail.com>
2025-03-15 04:55:41 -0700

    Update 4003.py
    new

--- 4003.py
+++ 4003.py
@@ -1,27 +1,27 @@
-#متغیر های رشته ای
-bookName = "Learning Python"
-the_auther = "Ali Salemi"
-publisher = "Iran ublications"
+# متغیرهای رشته‌ای
+book_name = "یادگیری پایتون"
+author_name = "علی سالمی"
+publisher = "انتشارات ایران"
 
-#متغیر های عددی
+# متغیرهای عددی
 pages = 350
 year_of_publication = 2024
 price = 50000
 
-#متغیر های منطقی
+# متغیرهای منطقی
 is_available = True
-it_has_a_transalator = False
-the_book_is_colored = True
+has_translator = False
+is_colored_book = True
 
 # استفاده از متغیرها برای نمایش اطلاعات
-print("bookName:" + bookName)
-print("the_auther:" + the_auther)
-print("publisher:" + publisher)
+print("نام کتاب: " + book_name)
+print("نام نویسنده: " + author_name)
+print("ناشر: " + publisher)
 
-print("pages:" + str(pages))
-print("year_of_publication:" + str(year_of_publication))
-print("price:" + str(price))
+print("تعداد صفحات: " + str(pages))
+print("سال انتشار: " + str(year_of_publication))
+print("قیمت: " + str(price))
 
-print("is_available:" + str(is_available))
-print("it_has_a_transalator:" + str(it_has_a_transalator))
-print("the_book_is_colored:" + str(the_book_is_colored))
+print("موجود است: " + str(is_available))
+print("مترجم دارد: " + str(has_translator))
+print("کتاب رنگی است: " + str(is_colored_book))
