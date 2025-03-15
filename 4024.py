dc221a73214c4f7d97697d1982f8901fb8c282ed
asp2006 <proud.boy2006@gmail.com>
2025-03-15 05:47:04 -0700

    Update 4024.py

--- 4024.py
+++ 4024.py
@@ -1,35 +1,42 @@
 # پیمایش بر روی یک لیست
-name = ["ali","sahar","sara"]
+name = ["علی", "سحر", "سارا"]
 for name in name:
     print(name)
+
 # استفاده از range() برای تولید اعداد
 for i in range(5):
     print(i)
-#  پیمایش بر روی دیکشنری
-dict = {"name": "rose", "age": 19, "city": "New York"}
+
+# پیمایش بر روی دیکشنری
+dict = {"نام": "رز", "سن": 19, "شهر": "نیویورک"}
 for key in dict:
     print(key, ":", dict[key])
+
 # استفاده از enumerate() برای دریافت اندیس و مقدار
-colors = ["red", "green","blue","black"]
-for index, colors in enumerate(colors):
-    print(index,colors)
-# استفاده از break و continue
+colors = ["قرمز", "سبز", "آبی", "مشکی"]
+for index, color in enumerate(colors):
+    print(index, color)
+
+# استفاده از break برای خروج از حلقه
 for i in range(10):
-    if i == 5 :
+    if i == 5:
         break
     print(i)
-# continue
+
+# استفاده از continue برای پرش به مرحله بعدی
 for i in range(17):
-    if i % 2 ==1 :
+    if i % 2 == 1:
         continue
     print(i)
-word = "ananas"
+
+word = "آناناس"
 for letter in word:
-   if letter =="a":
-       continue
-   print(letter) 
+    if letter == "ا":
+        continue
+    print(letter)
+
 # استفاده از continue در شرایط پیچیده‌تر
-numbers = [2, 5, 12, 18, 9, 39, 11,  32, 21]
+numbers = [2, 5, 12, 18, 9, 39, 11, 32, 21]
 for num in numbers:
     if num < 10 or num % 2 == 0:
         continue
