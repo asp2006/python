684c3571754bad220fcfb89dac340ed11b8988da
asp2006 <proud.boy2006@gmail.com>
2025-03-15 08:03:40 -0700

    Update 4043.py
    new

--- 4043.py
+++ 4043.py
@@ -2,7 +2,7 @@
 import pdfplumber
 
 # باز کردن فایل PDF
-with pdfplumber.open(r"C:\Users\kara\OneDrive\Desktop\python\ASP\leo.pdf") as pdf:
+with pdfplumber.open(r"C:\Users\kara\OneDrive\Desktop\python\ASP\story.pdf") as pdf:
     for page_num in range(len(pdf.pages)):
         page = pdf.pages[page_num]
         text = page.extract_text()
